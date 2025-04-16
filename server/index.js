a889836b8df13b1a872cff22fe36823db3c87401
guganraj-dev <guganrajofficial@gmail.com>
2025-04-16 23:35:13 +0530

    commited

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 app.get('/',(req,res)=>{
     useModel.find({})
